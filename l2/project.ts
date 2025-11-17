0505d0ee99947f0b90f5aa3914dca6742730b607
Lucas Mafra <36516904+lucasexpansiva@users.noreply.github.com>
2025-11-17 09:17:58 -0300

    Add project configuration in project.ts
    ajuste após delete nos arquivos temporários

---
+++ l2/project.ts
@@ -0,0 +1,15 @@
+/// <mls shortName="project" project="102022" enhancement="_100554_enhancementLit" groupName="other" />
+export const projectConfig = {
+    masterFrontEnd: {
+        build: '_102020_build',
+        start: '_102020_start',
+        liveView: '_102020_collabAuraLiveView',
+    },
+    masterBackEnd: {
+        build: '_102021_buildServer',
+        start: '_102021_start',
+        serverView: '_102021_liveServer'
+    },
+    modules: [      
+    ]
+}
