29cd74c78d7fe1c8db3c08e054bb3d81fb7e4120
Lucas Mafra <36516904+lucasexpansiva@users.noreply.github.com>
2025-12-10 14:45:45 -0300

    ajustes em project, no root, e no moduo utils

--- l2/utils/module.test.ts
+++
@@ -1,5 +0,0 @@
-/// <mls shortName="module" project="102022" enhancement="_blank" folder="utils" />
-
- import { ICANTest, ICANIntegration, ICANSchema  } from '/_100554_/l2/tsTestAST.js';
- export const integrations: ICANIntegration[] = [];
- export const tests: ICANTest[] = [];
