e963e3060b5fea5cc546f51d365b732d4db4408c
Lucas Mafra <36516904+lucasexpansiva@users.noreply.github.com>
2025-12-16 11:44:26 -0300

    ajustes nos agentes e miniaplicativos

--- l2/areaoftest/module.test.ts
+++
@@ -1,5 +0,0 @@
-/// <mls shortName="module" project="102022" enhancement="_blank" folder="areaoftest" />
-
- import { ICANTest, ICANIntegration, ICANSchema  } from './_100554_tsTestAST';
- export const integrations: ICANIntegration[] = [];
- export const tests: ICANTest[] = [];
