0e235ddeac28c86a0c34c6b30212dfb790343316
Lucas Mafra <36516904+lucasexpansiva@users.noreply.github.com>
2025-12-19 09:56:44 -0300

    ajustes em arquivos do wwwroot

--- l2/collabRunTime.ts
+++ l2/collabRunTime.ts
@@ -1,8 +1,9 @@
-/// <mls shortName="collabRunTime" project="102022" enhancement="_blank" />
+/// <mls shortName="collabRunTime" project="102022" enhancement="_100554_enhancementLit" folder="" />
 
-(window as any).originalDefine = customElements.define.bind(customElements);
+(window as any)['originalDefine'] = customElements.define.bind(customElements);
 customElements.define = (name, constructor, options) => {
-    if (!customElements.get(name)) {
-        return (window as any).originalDefine(name, constructor, options);
-    }
+  if (!customElements.get(name)) {
+    return (window as any)['originalDefine'](name, constructor, options);
+  }
 };
+
